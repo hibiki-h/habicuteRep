5326bc2085f5fab8bb7bd7dd8a0b8ad32f02b9f7
hibiki-h <h.hatori0603@gmail.com>
2025-06-09 18:14:59 +0900

    commit_20250609

--- backend/habicuteproject/habicuteproject/settings.py
+++ backend/habicuteproject/habicuteproject/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'habicuteproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "habicutedb",
+        "USER": "hibiki",
+        "PASSWORD": "hibikipassword",
+        "HOST": "172.26.0.3",
+        "PORT": "5432",
     }
 }
 
--- frontend/habicute_react_vite/src/api/todoListsApi.ts
+++ frontend/habicute_react_vite/src/api/todoListsApi.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const API_BASE_URL = apiUrl;
-console.log(`apiUrl log : ${apiUrl}`)
 const AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 5000,

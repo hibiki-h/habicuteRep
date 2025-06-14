ca6d8c17aa64cacda3af13a70eaf3b877d97acc7
hibiki-h <h.hatori0603@gmail.com>
2025-06-14 23:02:17 +0900

    commit_20250614v2

--- backend/habicuteproject/habicuteproject/settings.py
+++ backend/habicuteproject/habicuteproject/settings.py
@@ -94,10 +94,14 @@ WSGI_APPLICATION = 'habicuteproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=env("DATABASE_URL"),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
 }
 
 # Password validation
